82df00d58d827e9bcd142be9aacd6d78f26b050b
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2022-10-09 18:07:09 -0300

    Mudança no texto da saída
    Coloquei o meu CPF no lugar do meu número de matrícula

--- LAB01 - Procedural/ex01/HelloWorld.java
+++ LAB01 - Procedural/ex01/HelloWorld.java
@@ -1,6 +1,6 @@
  public class HelloWorld {
     public static void main(String args[]) {
 	 
-     System.out.printf("Hello, world!\n11389175677\n");
+     System.out.printf("Hello, world!\n12111BSI203\n");
     }
 }
