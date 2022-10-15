afbb2304267b35c3e18977f43507b3401c197e51
Eduardo Pires <duduspiress@hotmail.com>
2022-10-15 20:05:33 -0300

    Exercício 2 - Finalização e comentários
    fiz mudanças pequenas na saída do código para melhor leitura e entendimento, o funcionamento continua o mesmo do exemplo 4 e todas estão comentadas

--- LAB01 - Procedural/ex02/Ex2_Exemplo04/src/Main.java
+++ LAB01 - Procedural/ex02/Ex2_Exemplo04/src/Main.java
@@ -7,10 +7,17 @@ public class Main
         Produto[] item = new Produto[4];
         Scanner scanf = new Scanner(System.in);
 
+        /*  realizei pequenas mudanças estéticas na saída do programa, porém os valores e funcionalidade continuam os
+         *  mesmos do "exemplo 04.c"
+         */
+
         for(int i = 0; i < 4; i++)
         {
             item[i] = new Produto();
 
+            /*  adicionei o índice do produto na saída para dar uma exemplificação melhor de qual produto está sendo
+             *  inserido no momento.
+             */
             System.out.printf("\nInforme o nome, o preco e a qtd. em estoque do produto %d:\n", i+1);
             item[i].nome = scanf.nextLine();
             item[i].preco = scanf.nextFloat();
@@ -19,7 +26,10 @@ public class Main
             scanf.nextLine();
         }
 
-        System.out.printf("\nAumentando o preco em 10%% do produto 1 e 3");
+        //nas saídas eu dei uma otimizada nos espaços para melhor vizualização
+
+        System.out.printf("\nAumentando o preco em 10%% do produto 1 e 3\n");
+        //acredito que possa ter um equivoco nessa parte, mas mantive os valores do original
         item[0].altera_preco(item[0].preco, -110);
         item[2].altera_preco(item[2].preco, 10);
 
@@ -35,6 +45,7 @@ public class Main
         }
 
         System.out.printf("\n\nAlterando o preco do prod. 3");
+        //outra vez acredito que possa ter tido um equívoco, pois está alterando o quarto produto, mas mantive como estava
         if (!(item[3].altera_preco(item[3].preco,-110)))
         {
             System.out.printf("\nErro: preco nao alterado. Porcentagem invalida\n");
@@ -42,7 +53,8 @@ public class Main
 
         System.out.printf("\nProdutos Cadastrados:");
         for (int i = 0; i < 4; i++){
-            System.out.printf("\nProduto: %s\nPreco: %f\nEstoque: %d\n", item[i].nome, item[i].preco, item[i].qtd_estoque);
+            //alteração no número de casas mostradas após a vírgula, apenas por convenção estética
+            System.out.printf("\nProduto: %s\nPreco: %.2f\nEstoque: %d\n", item[i].nome, item[i].preco, item[i].qtd_estoque);
         }
 
     }
