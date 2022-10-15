a8a073f7e8a169f3b178584d3a3ea6c9c849d6ff
Eduardo Pires <duduspiress@hotmail.com>
2022-10-15 15:31:53 -0300

    entrada melhorada e novos erros no código achados
    a leitura das variáveis está ok, porém não notei que o preço não estava sendo alterado

--- LAB01 - Procedural/ex02/Ex2_Exemplo04/src/Main.java
+++ LAB01 - Procedural/ex02/Ex2_Exemplo04/src/Main.java
@@ -4,13 +4,12 @@ public class Main
 {
     public static void main(String[] args)
     {
-
         Produto item = new Produto();
         Scanner scanf = new Scanner(System.in);
 
         for(int i = 0; i < 4; i++)
         {
-            System.out.printf("\nInforme o nome, o preco e a qtd. em estoque do produto 1:\n");
+            System.out.printf("\nInforme o nome, o preco e a qtd. em estoque do produto %d:\n", i+1);
             item.nome[i] = scanf.nextLine();
             item.preco[i] = scanf.nextFloat();
             item.qtd_estoque[i] = scanf.nextInt();
@@ -24,28 +23,25 @@ public class Main
 
         System.out.printf("\nReduzindo o preco em 5%% do produto 2");
 
-        if(item.altera_preco(item.preco[1], -5))
+        if(item.altera_preco(item.preco[1], -5) == false)
         {
-            System.out.printf("\n\nErro: preco nao alterado. Porcentagem invalida\n");
+            System.out.printf("\nErro: preco nao alterado. Porcentagem invalida");
         }
         else
         {
-            System.out.printf("\n\n Preco alterado com sucesso");
+            System.out.printf("\nPreco alterado com sucesso");
         }
 
-        System.out.printf("\nAlterando o preco do prod. 3");
-
-        System.out.printf("\nAlterando o preco do prod. 3");
+        System.out.printf("\n\nAlterando o preco do prod. 3");
         if (item.altera_preco(item.preco[3],-110) == false)
         {
-            System.out.printf("\n\nErro: preco nao alterado. Porcentagem invalida\n");
+            System.out.printf("\nErro: preco nao alterado. Porcentagem invalida\n");
         }
 
-        System.out.printf("\nProdutos Cadastrados:\n");
+        System.out.printf("\nProdutos Cadastrados:");
         for (int i = 0; i < 4; i++){
-            System.out.printf("\nProduto: %s\nPreco: %.2f\nEstoque: %d\n", item.nome[i],item.preco[i],item.qtd_estoque[i]);
+            System.out.printf("\nProduto: %s\nPreco: %f\nEstoque: %d\n", item.nome[i],item.preco[i],item.qtd_estoque[i]);
         }
-
 
     }
 }
--- LAB01 - Procedural/ex02/Ex2_Exemplo04/src/Produto.java
+++ LAB01 - Procedural/ex02/Ex2_Exemplo04/src/Produto.java
@@ -6,14 +6,15 @@ public class Produto {
     public float[] preco = new float[4];
     public int[] qtd_estoque = new int[4];
 
-    public boolean altera_preco(float preco1, float porcentagem)
+    public boolean altera_preco(float preco, float porcentagem)
     {
         float temp;
 
         if(porcentagem > -100)
         {
             temp = 1 + porcentagem/100;
-            preco1 = preco1*temp;
+            //O this.preco não funciona e sem ele o código roda, porém não troca o preço
+            this.preco = preco * temp;
             return true;
         }
         else
