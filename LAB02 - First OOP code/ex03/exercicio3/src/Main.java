7fd5150de2926825b86cec86b8b17f821af1c032
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2022-11-05 22:00:34 -0300

    ex03 -adaptação ao novo objeto
    adicionei o novo objeto figurinha e adaptei o código da main á atualização do objeto Figurinha

--- LAB02 - First OOP code/ex03/exercicio3/src/Main.java
+++ LAB02 - First OOP code/ex03/exercicio3/src/Main.java
@@ -5,19 +5,42 @@ public class Main {
     {
         Scanner scanf = new Scanner(System.in);
 
+
         System.out.print("Digite o numero de jogadores: ");
         int numJog = scanf.nextInt();
-        Figurinha[] jog = new Figurinha[numJog];
+        Figurinha[] jogador = new Figurinha[numJog];
+        scanf.nextLine();
 
+        //informações do jogador adicionadas na entrada pelo usuário
         for (int i = 0; i < numJog; i++)
         {
-            jog[i] = new Figurinha();
-            jog[i].alteraInfo(jog[i]);
+            jogador[i] = new Figurinha();
+            
+            System.out.printf("\n<<entre com as informações do jogador %d>>\n", i+1);
+            System.out.print("Nome do jogador: ");
+            jogador[i].setName(scanf.nextLine());
+
+            System.out.print("data de nascimento do jogador [ano-mês-dia]: ");
+            jogador[i].setbirthDate(scanf.nextLine());
+
+            System.out.print("altura do jogador: ");
+            jogador[i].setHeight(scanf.nextFloat());
+
+            System.out.print("peso do jogador: ");
+            jogador[i].setWeight(scanf.nextFloat());
+
+            scanf.nextLine();
+
+            System.out.print("posição do jogador: ");
+            jogador[i].setPosition(scanf.nextLine());
+
+            System.out.print("país do jogador: ");
+            jogador[i].setCountry(scanf.nextLine());
         }
 
         for (int i = 0; i < numJog; i++)
         {
-            jog[i].mostraInfo(jog[i]);
+            jogador[i].printFigurinha();
         }
 
     }
