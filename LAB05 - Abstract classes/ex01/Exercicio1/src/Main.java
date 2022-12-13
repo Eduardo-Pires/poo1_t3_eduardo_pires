908307731db12fe3581283393d41a54fb985ec84
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2022-12-13 16:36:07 -0300

    classes abstratas - exercício 1 terminado

--- LAB05 - Abstract classes/ex01/Exercicio1/src/Main.java
+++ LAB05 - Abstract classes/ex01/Exercicio1/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+        ContaCorrente conta = new ContaCorrente(2500, 4587, 123456);
 
+        conta.debitaValor(255, 123456);
+        conta.debitaValor(123.45f);
+
+        System.out.println("saldo = " + conta.getSaldo(123456));
+
+        conta.creditaValor(123456, 312);
+
+        System.out.println("a senha é 46546? ");
+
+        if(conta.isSenha(46546))
+        {
+            System.out.println("a senha está correta");
+        }
+        else
+        {
+            System.out.println("a senha está incorreta");
+        }
+
+        System.out.println("saldo = " + conta.getSaldo(123456));
+        conta.debitaValor(2433.55f, 123456);
+        System.out.println("saldo = " + conta.getSaldo(123456));
+        System.out.println("é possível retirar mais? "  + conta.debitaValor(2433.55f, 123456));
     }
 }
