96c625232cc6882eff2a8f97fa4919d72ee81aad
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2022-12-13 21:06:23 -0300

    classes abstratas - exercício 2 base

---
+++ LAB05 - Abstract classes/ex02/Exercicio2/src/ContaComum.java
@@ -0,0 +1,7 @@
+public class ContaComum extends ContaCorrente
+{
+    public ContaComum(float val, int num, int pwd) {
+        super(val, num, pwd);
+    }
+
+}
---
+++ LAB05 - Abstract classes/ex02/Exercicio2/src/ContaEspecial.java
@@ -0,0 +1,12 @@
+public class ContaEspecial extends ContaCorrente
+{
+    private float limite;
+    public ContaEspecial(float val, int num, int pwd, float limite)
+    {
+        super(val, num, pwd);
+        this.limite = limite;
+    }
+
+
+
+}
